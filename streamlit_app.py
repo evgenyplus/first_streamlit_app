5390f533d5ecd86e7fbb0fac150a899bae792226
evgenyplus <evgenyplus@gmail.com>
2022-07-11 12:28:15 +0300

    Update streamlit_app.py
    aa menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents Mew Healthy Diner')
+streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Brekfast Menu')
+streamlit.text('Omega 3 & Blubery oatmeal')
+streamlit.text('Kale, spinach & rocket smoothie')
+streamlit.text('Hard-bolied free-range egg')
