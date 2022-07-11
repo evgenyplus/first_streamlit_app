457c91281d86d8f1b10cfcc84d49d1494b755087
evgenyplus <evgenyplus@gmail.com>
2022-07-11 15:22:34 +0300

    Update streamlit_app.py
    add icon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Brekfast Menu')
-streamlit.text('Omega 3 & Blubery oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-bolied free-range egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
